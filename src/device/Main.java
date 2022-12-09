a776ffbaa8aeb837b69002bb0a2fb8218eebd76f
Minisiia <ar_tik@yahoo.com>
2022-12-09 22:14:49 +0200

    Java Essential 8. Class Object

---
+++ src/device/Main.java
@@ -0,0 +1,10 @@
+package device;
+
+public class Main {
+    public static void main(String[] args) {
+        Device device = new Device("Samsung",120, "AB1234567CD");
+        Monitor monitor = new Monitor("Samsung",120, "AB1234567CD",1280,1024);
+        System.out.println(device);
+        System.out.println(monitor);
+    }
+}
---
+++ src/device/Monitor.java
@@ -0,0 +1,46 @@
+package device;
+
+public class Monitor extends Device{
+    private int resolutionX;
+    private int resolutionY;
+
+    public Monitor() {
+    }
+
+    public Monitor(int resolutionX, int resolutionY) {
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
+    }
+
+    public Monitor(String manufacturer, float price, String serialNumber, int resolutionX, int resolutionY) {
+        super(manufacturer, price, serialNumber);
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
+    }
+
+    public int getResolutionX() {
+        return resolutionX;
+    }
+
+    public void setResolutionX(int resolutionX) {
+        this.resolutionX = resolutionX;
+    }
+
+    public int getResolutionY() {
+        return resolutionY;
+    }
+
+    public void setResolutionY(int resolutionY) {
+        this.resolutionY = resolutionY;
+    }
+
+    @Override
+    public String toString() {
+        return "Device:" +
+                "manufacturer=" + super.getManufacturer() +
+                ", price=" + super.getPrice() +
+                ", serialNumber=" + super.getSerialNumber() +
+                ", X=" + resolutionX +
+                ", Y=" + resolutionY;
+    }
+}
